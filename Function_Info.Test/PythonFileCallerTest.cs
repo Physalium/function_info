b7ad679ddb25e1a438b6d2b7fa22d9603d2857f4
Karol <physalium2@gmail.com>
2020-05-27 00:52:00 +0200

    Changed test names to more representative ones

---
+++ Function_Info.Test/PythonFileCallerTest.cs
@@ -0,0 +1,58 @@
+using System;
+using System.IO;
+
+using Function_Info.Model;
+
+using Xunit;
+
+namespace Function_Info_Test
+{
+
+    public class PythonCallerTest
+    {
+        private PythonFileCaller model = new PythonFileCaller();
+
+        [Fact]
+        public void PythonCallerProperlyCalls()
+        {
+            string workingDirectory = Directory.GetParent(Environment.CurrentDirectory).FullName;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
+            var outputDir = Path.GetFullPath(Path.Combine(projectDirectory, @"..\Function_Info\Model\"));
+            var testPath = outputDir + @"Test.txt";
+            model.CreateGraph("test", "0", "0");
+
+            string text;
+            try
+            {
+                text = File.ReadAllText(testPath);
+            }
+            catch (System.Exception)
+            {
+                System.Console.WriteLine(outputDir);
+                text = string.Empty;
+            }
+            finally
+            {
+                File.Delete(testPath);
+            }
+            var expected = "test";
+            Assert.Equal(expected, text);
+        }
+
+        [Fact]
+        public void TestGraphIsCreated()
+        {
+            string workingDirectory = Directory.GetParent(Environment.CurrentDirectory).FullName;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
+            var outputDir = Path.GetFullPath(Path.Combine(projectDirectory, @"..\Function_Info\Model\"));
+
+            var formula = "x**2";
+            var lBound = "-10";
+            var rBound = "10";
+            var graphFile = Path.Combine(outputDir, "graph.png");
+            model.CreateGraph(formula, lBound, rBound);
+            Assert.True(File.Exists(graphFile));
+        }
+    }
+
+}
